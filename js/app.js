b951b7c676edf96ad88cce5982f390036f515fba
SvenMayCry <svenmaycry@gmail.com>
2024-06-06 09:57:27 +0300

    Добавит страницу блогеров и одного блогера.

--- js/app.js
+++ js/app.js
@@ -656,6 +656,53 @@ formMessage.addEventListener('blur', onBlurFormMessage);
 formMessage.addEventListener('input', onMessageInput);
 formName.addEventListener('input', onNameInput);
 
+//=============================СВАЙПЕР СТРАНИЦЫ БЛОГЕРА==================================
+const videoSwiper = new Swiper('.js-swiper-blogger-videos', {
+  // Optional parameters
+  observer: true,
+  observeParents: true,
+  loop: true,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 20,
+  speed: 600,
+  parallax: true,
+  preloadImages: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    autoHeight: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Брейкпоинты
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    992: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1268: {
+      slidesPerView: 4,
+    },
+  }
+
+});
 
 
 
